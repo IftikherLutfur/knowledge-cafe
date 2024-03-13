cc84c3c27fbae862c6fbde6e4663774e4353ef8c
iftikhrlutfur <iftikherlutfur@gmail.com>
2024-03-14 04:55:53 +0600

    Adde the useState and useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header/Header'
+import Blogs from './components/blog/blogs'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-     <h1 className='text-4xl bg-orange-400 p-5' >Knowledge Cafe</h1>
+    <Header></Header>
+    <Blogs></Blogs>
     </>
   )
 }
